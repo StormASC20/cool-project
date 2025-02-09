7124d70bc7ae7af7b950c9425093c60d2bd33b84
StormASC20 <stormrivera953@yahoo.com>
2025-02-09 14:57:24 -0500

    moved stuff around

--- pastromi/JSON/webdata/nothingtosee/turnback/confetti.js
+++
@@ -1,9 +0,0 @@
-import confetti from 'https://cdn.skypack.dev/canvas-confetti';
-
-function makeConfetti(){
-    confetti()
-}
-
-let yeah = document.querySelector("#yeah-button");
-
-    yeah.addEventListener("click", () => {makeConfetti()});
